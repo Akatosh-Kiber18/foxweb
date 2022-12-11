4a564ad328f5aa674c4b44f6fb984d0cb36087fe
Laredef <cemper300@gmail.com>
2022-12-11 22:01:17 +0200

    Add new buttons and fill contacts block

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -5,13 +5,19 @@ export const Contacts = () => {
     return (
         <div className={styles.contactsContainer} id={'contacts'}>
             CONTACTS
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
+            
+            <ul>
+                <li>
+                <a href="https://www.instagram.com/easy_english_foxx/" target="_blank"  rel="noreferrer">instagram</a>
+                </li>
+                <li>
+                <a href="https://t.me/englishfoxx/" target="_blank"  rel="noreferrer">telegram</a>
+                </li>
+                <li>
+                <a href="https://www.facebook.com/englishfoxx" target="_blank"  rel="noreferrer">facebook</a>
+                </li>
+                <li>email: test.email@google.com</li>
+            </ul>
         </div>
     );
 };
